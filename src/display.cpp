fd094946f6a7afa8671ab393f507532a9b466424
imim77 <mislav.vusic@gmail.com>
2026-05-19 20:03:38 +0200

    robo eyes

--- src/display.cpp
+++ src/display.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <FluxGarage_RoboEyes.h>
 #include <Wire.h>
 #include "env_config.h"
 #include "display.h"
+
+#ifndef OLED_SDA
+#define OLED_SDA 21
+#endif
+
+#ifndef OLED_SCL
+#define OLED_SCL 22
+#endif
 
 #ifndef OLED_ADDR
 #define OLED_ADDR 0x3C
@@ -17,9 +26,15 @@
 #define OLED_HEIGHT 64
 #endif
 
+#ifndef ROBOEYES_FPS
+#define ROBOEYES_FPS 30
+#endif
+
 static Adafruit_SSD1306 oled(OLED_WIDTH, OLED_HEIGHT, &Wire, -1);
+static RoboEyes<Adafruit_SSD1306> eyes(oled);
 
 static void displayTask(void *parameters) {
+  Wire.begin(OLED_SDA, OLED_SCL);
 
   if (!oled.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
     Serial.println("[OLED] init failed");
@@ -27,16 +42,14 @@ static void displayTask(void *parameters) {
     return;
   }
 
-  oled.clearDisplay();
-  oled.setTextSize(2);
-  oled.setTextColor(SSD1306_WHITE);
-  oled.setCursor(0, 20);
-  oled.println("Hello");
-  oled.println("World!");
-  oled.display();
+  eyes.begin(OLED_WIDTH, OLED_HEIGHT, ROBOEYES_FPS);
+  eyes.setAutoblinker(ON, 3, 2);
+  eyes.setIdleMode(ON, 2, 2);
+  eyes.setMood(DEFAULT);
 
   for (;;) {
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    eyes.update();
+    vTaskDelay(1 / portTICK_PERIOD_MS);
   }
 }
 
